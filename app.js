5d2f3f1ac1a3a05c71e2bd35e403f29a6a42e271
Nadeem Hussain <hoteleyenadeem@gmail.com>
2021-07-01 16:10:05 +0500

    javascript test code

---
+++ app.js
@@ -0,0 +1,535 @@
+                                            // ch-01 Alert test
+
+// Q-no : 01
+// 1. Write a script to greet your website visitor using JS alert box
+
+// Answer ......
+// alert("Hello World");  
+
+// Q-no : 02
+// 2. Write a script to display following message on your web page:
+
+// Answer .....
+// alert("Error! Please enter a valid password.");   
+
+// Q-no : 03
+// 3. Write a script to display following message on your web page:
+
+// Answer.....
+// alert("Welcome to JS Land...\nHappy Coding!");    3
+
+// Q-no : 04
+// 4. Write a script to display following messages in sequence:
+
+// Answer....
+// alert("Welcome to JS Land...")   4
+// alert("Happy Coading!\n Prevent this page from creating additional dialogs.")   4
+
+// Q-no : 05
+// 5. Generate the following message through browser’s developer console:
+
+// Answer....
+/* var a = "Hello... I can run JS through my web browser's console";
+alert(a);
+console.log(a); */
+          
+// Q-no : 06
+// 6. Make use of alerts in your new/existing HTML & CSS project.
+// new existing HTML file index.html project   6
+// script element C. (Body (inside your page’s HTML)   7
+
+// ch-01 Complete 
+
+
+                                // ch-02 VARIABLES FOR STRINGS 
+// Q-no : 01
+// 1. Declare a variable called username.
+
+// Answer no 1
+//  var username;
+
+// Q-no : 02
+// 2. Declare a variable called myName & assign to it a string
+
+//   Answer no 2
+//  var myName = "Nadeem Hussain";
+
+// Q-no : 03
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// Answer no 3
+//  var message ="Hello World";
+// alert(message);
+
+// Q-no : 04
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+// Anawer no 4
+// var myName = "Nadeem Hussain";
+// alert(myName);
+// var age = 23;
+// alert(age);
+// var course = "Ceritified Mobile Application Development";
+// alert(course);
+
+// Q-no : 05
+// 5. Write a script to display the following alert using one JS variable:
+
+//  Answer no 5
+// var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(pizza);
+
+// Q-no : 06
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+
+// Answer no 6
+//  var email = "My email address is hoteleyenadeem@gmail.com";
+// alert(email);
+
+// Q-no : 07
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+
+// Answer no 7
+// var book = "i am trying to learn from the book A smarter way to learn JavaScript."
+// alert(book);
+
+// Q-no : 08
+// 8. Write a script to display this in browser through JS
+
+// Anser no 8
+// var a = "Yah! i can write HTML content through JavaScript";
+// document.write(a);
+
+// Q-no : 09
+// 9. Store following string in a variable and show in alert and browser through JS
+
+// Answer no 9
+// var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(a);
+
+
+                                                                  //   CHAPTER NO 3  VARIABLES FOR NUMBERS 
+// Q-no : 01 
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.  
+// var age = "i am 15 year old"
+// alert(age);                                                                                    
+
+
+
+// Q-no : 02
+// 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+
+// Answer no 2
+// alert("You have visited this site 14 times ");
+
+// Q-no : 03
+// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+
+// Answer no 3
+// var birthYear = "My birth year is 1996";
+// document.write(birthYear);
+
+// Q-no : 04
+// 4. A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// Answer no 4
+
+// var name = "Nadeem Hussain ";
+// var item = " j. "
+// var quan  = "5 T-shirts "
+// var store = "J. Store ";
+// var total = name + item + quan + store ;
+// document.write(total);
+
+
+                                                // ch-04 VARIABLE NAMES: LEGAL & ILLEGAL 
+
+// Q-no : 01
+// 1. Declare 3 variables in one statement
+
+// Answer....
+// var myName = "Nadeem ", lastName = "hussain ", age = "23"; 
+// alert(myName + lastName + age);
+
+// Q-no : 02
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// Answer.... 
+// legal variable
+// var a ;
+// var _marks ;
+// var myName ;
+// var $sign ;
+// var result ;
+
+// illegal variable
+// var  1num ;
+// var %mode ;
+// var My name ;
+// var &sign ;
+// var @sign ;
+
+
+
+
+
+
+                                                // ch-05 MATH EXPRESSIONS
+
+// Q-no: 01 
+// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+
+// Answer...........   add no
+// var a = 3;
+// var b = 5;
+// var c = a+b;
+// document.write("sum of 3 and 5 is "+ c);
+
+// Q-no : 02 
+// 2. Repeat task1 for subtraction, multiplication, division & modulus.
+ 
+// Answer...........   subtraction no
+// var a = 5;
+// var b = 3;
+// var c = a - b;
+// document.write(c);
+                        
+                    // multiplication
+// var a = 5;
+// var b = 3;
+// var c = a * b;
+// document.write(c);
+                    //division
+// var a = 5;
+// var b = 3;
+// var c = a / b;
+// document.write(c);
+                    //modulus
+// var a = 5;
+// var b = 3;
+// var c = a % b;
+// document.write(c);
+
+// Q-no : 03 
+/*
+3. Do the following using JS Mathematic Expressions
+a. Declare a variable.
+b. Show the value of variable in your browser like “Value
+after variable declaration is: ??”.
+c. Initialize the variable with some number.
+d. Show the value of variable in your browser like “Initial
+value: 5”.
+e. Increment the variable.
+f. Show the value of variable in your browser like “Value
+after increment is: 6”.
+g. Add 7 to the variable.
+h. Show the value of variable in your browser like “Value
+after addition is: 13”.
+i. Decrement the variable.
+j. Show the value of variable in your browser like “Value
+after decrement is: 12”.
+k. Show the remainder after dividing the variable’s value
+by 3.
+l. Output : “The remainder is : 0”.
+ */
+
+// Answer...........
+// var a;
+// var a = 5;
+// var b = ++a;
+// var c = b+7;
+// var d = --c;
+// var e = d % 3;
+// document.write("The remainder is :" + e);
+
+// Q-no : 04 
+// 4. Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie.
+
+// Answer...........
+// var ticket = 600;
+// var tickets = ticket * 5;
+// document.write("Total cost to buy 5 tickets to a movie in " + tickets + "PKR");
+
+// Q-no : 05 
+
+// var num = +prompt("Table Of");
+// for(var a = 1; a<= 10; a++){
+//     document.write(num + " X " + a + " = " + num*a + "<br >");
+// }
+
+// Q-no : 06 
+
+
+
+// Q-no : 07 
+
+// var item1 = 650;
+// var price = item1 * 3;
+// document.write("Price of item 1 is "+ item1 + "<br >" , "Quantity of item 1 is 3" , "<br >" );
+
+// var item2 = 100;
+// var price2 = item2 * 7;
+// document.write("Price of item 2 is " + item2 + "<br >", "Quantity of item 2 is 7 " , "<br >");
+
+// var charges = 100;
+// document.write("Price of Shipping Charges is " + charges + "<br >" + "<br >");
+
+// var total = item1,item2,charges;
+// document.write("Total cost of your order is " , price + price2 + charges);
+
+// Q-no : 08 
+
+// var total_marks = 980;
+// var obtained_marks = 804;
+// document.write("Total marks: ", total_marks, "<br >", "Marks obtained: ", obtained_marks, "<br >", "Percentage : ", obtained_marks / total_marks * 100, "%");
+
+// Q-no : 09 
+
+// var usDollar = 10;
+// var usRates = usDollar*104.80;
+// var saudiRiyal = 25;
+// var sauRates = saudiRiyal*28;
+// console.log(usRates);
+// console.log(sauRates);
+// document.write("Total Currency in PKR: ",usRates + sauRates);
+
+// Q-no : 10 
+ 
+// var a = 5;
+// var b = a + 5;
+// var c = b * 10;
+// var d = c / 2;
+// document.write(d);
+
+// var a = 5, b= a+5, c= b*10, d= c/2;
+// document.write(d);
+// console.log(d);
+
+// Q-no : 11 
+
+// var currentYear  = 2021;
+// var birthYear = 1996;
+// var age  = currentYear - birthYear;
+// document.write('Your Age is: ' + age);
+
+// Q-no : 12 
+
+// var radius = 20;
+// var circumference = Math.PI * 2*radius;
+// document.write("Radius of a circle: " , radius , "<br >","The circumference is " + circumference + "<br >");
+// var area = Math.PI * radius*radius;
+// document.write("The area is " + area);
+
+// Q-no : 13 
+
+// var age = 15;
+// var maxAge = 65;
+// var snacksPerDay = 3;
+// var totalNeeded = (snacksPerDay) * (maxAge - age);
+// var result = 'You will need ' + totalNeeded + ' chocolate chip to last you until the ripe old age of ' + maxAge;
+// document.write(result);
+
+
+                                                // ch-06 MATH EXPRESSIONS
+
+// Q-no : 01 
+// 1. Write a program to take a number in a variable, do the required arithmetic to display the following result in your browser:
+
+// Answer....
+// var a = 10;
+// var b = ++a;
+// console.log(b);
+// var c = b++;
+// console.log(c);
+// var d = --c;
+// console.log(d);
+// var e = d--;
+// document.write(e);
+
+// Q-no : 02 
+// 2. What will be the output in variables a, b & result after execution of the following script:
+
+// Answer......
+// var a = 2, b = 1;
+// console.log(a,b);
+// var result = --a - --b + ++b +b--;
+// console.log(result); 
+
+// Q-no : 03 
+// 3. Write a program that takes input a name from user & greet the user.
+
+// Answer....
+// userInput = prompt('Enter Your name','');
+// alert(' Hello ' + userInput);
+
+// Q-no : 05 
+// 5. Write a program to take input a number from user & display it’s multiplication table on your browser. If user does not enter a new number, multiplication table of 5 should be displayed by default.
+
+// Answer......
+// var a = +prompt("Enter Table value");
+// for(var i = 1; i<= 10; i++){
+//     document.write(a + " X "+ i + " = " + i*a + " <br >")
+//     }
+
+
+
+
+// ch-09 to 10 USER INPUT & CONDITIONAL STATEMENT
+
+// Q-no : 01 
+// 1. Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+
+// Answer..........
+
+// userInput = prompt('please enter city name.....')
+// var city = "karachi";
+// if(city == userInput){
+//     alert("Welcome to city of lights");
+// }
+
+// Q-no : 02
+// 2. Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the user is female, give the message: Good Morning Ma’am.
+
+// Answer 
+
+// userInput = prompt('Please select your gender ....');
+// var gender = "male";
+// if(gender == userInput){
+//     alert('Good Morning Sir')
+// }
+// else { 
+//     alert("Good Morning Ma'am.")
+
+// }
+
+// Q-no : 03
+// 3. Write a program to take input color of road traffic signal from the user & show the message according to this table:
+
+// Answer..... 
+
+// userInput = prompt('Select Your color.....');
+// var color = "red";
+// if(color == userInput){
+//     alert("Must Stop")
+// }
+// else if(userInput == "yellow"){
+//     alert("Ready to move")
+// }
+// else(userInput == "green");{
+//     alert("Move now");
+// }
+
+// Q-no : 04 
+// 4. Write a program to take input remaining fuel in car (in litres) from user. If the current fuel is less than 0.25litres, show the message “Please refill the fuel in your car”
+
+// Answer .......
+// userInput = prompt('show remaining fuel ......');
+// var fuel = 0.25;
+// if(fuel > userInput){
+// alert("Please refill the fuel in your car");
+// }
+
+// Q-no : 07
+// 7. Guess game:Store a secret number (ranging from 1 to 10) in a variable. Prompt user to guess the secret number. a. If user guesses the same number, show “Bingo! Correct answer”. b. If the guessed number +1 is the secret number, show “Close enough to the correct answer
+
+//Answer .........
+// userInput = prompt('Enter Secret Number.....');
+// var num = 6;
+// if(num == userInput){
+//     alert("Bingo! Correct answer...")
+// }
+// else if(userInput == 7){
+//     alert("Close enough to the correct answer")
+// }
+
+// Q-no : 09 
+// 9. Write a program that checks whether the given input is an even number or an odd number
+
+//Answer ......
+// var number = prompt('Enter a number');
+// if(number % 2 == 0){
+//     alert("The number is even.")
+// }
+// else{
+//     alert("The number is odd.")
+// }
+
+
+
+
+
+
+
+
+                                            // ch-17 Arry and loop 
+
+// Q-no : 01 & 02 
+
+// var arr = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ];
+// for (var a = 0; a < 3; a++) {
+//     for (var b = 0; b< 4; b++) {
+//         document.write(arr[a][b] + " ")
+//     }
+//     document.write("<br>")
+// }
+
+// Q-no : 03 
+
+// for(var a = 1; a<11 ;a++){
+//     document.write(a + "<br >");
+// } 
+
+// Q-no : 04
+
+// var a = +prompt("Enter Table value");
+// var b = +prompt("Enter Length value !");
+// for(var i = 1; i<= b; i++){
+//     document.write(a + " X "+ i + " = " + i*a + " <br >")
+//     }
+
+// Q-no : 05
+
+// var fruit = ["apple ","banana ", "mango ", "orange ","strawberry "];
+// for(var i = 0; i<fruit.length; i++){
+//     document.write(fruit[i] + "<br>");
+// }
+
+// Q-no : 06 
+
+// var a ;
+// for(a = 1; a<16 ;a++){
+//     document.write(a + "<br>");
+// }
+
+// var num = [1,2,3,4,5,6,7,8,9,10];
+//     document.write(num.reverse());
+
+// var num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// document.write(num eventTarget());
+
+
+
+
+// Q-no : 07 
+
+// userInput = prompt("please enter value....");
+// var arr = ['cake','apple','cookie','chips','patties'];
+// for (var i = 0; i<arr.length; i++){
+//         if(userInput === arr)
+//         document.write(userInput);
+// }
